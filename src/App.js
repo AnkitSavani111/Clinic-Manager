c6b4930efc60f5e3f8209450c4685a2c27137fa8
JaivinSavaliya <jpsavaliya14@gmail.com>
2023-07-30 11:53:15 +0530

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import Login from './Components/Login';
 import Home from './Components/Home';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="text-6xl">
-        <Home />
-
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
