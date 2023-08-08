7c6a3082317b32a7050bf40cb2382d34d572172c
JaivinSavaliya <jpsavaliya14@gmail.com>
2023-08-08 21:26:16 +0530

    before pull

--- src/Components/ReceptionDashBoard.js
+++ src/Components/ReceptionDashBoard.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import RecNav from "./RecNav";
+import RecSide from "./RecSide";
+import RecBody from "./RecBody";
 
 const ReceptionaDashBoard = () => {
   return (
     <>
-        
+    <div className="">
+      <RecNav />
+      <div className="">
+        <RecSide />
+        <RecBody />
+      </div>
+    </div>
     </>
-  )
-}
+  );
+};
 
-export default ReceptionaDashBoard
+export default ReceptionaDashBoard;
