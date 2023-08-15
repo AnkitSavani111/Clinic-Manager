93770d770a946062a79b9a5c8e87884997929269
JaivinSavaliya <jpsavaliya14@gmail.com>
2023-08-15 13:59:05 +0530

    Data fetching GET and POST

--- src/Components/Pages/ReceptionDashBoard.js
+++ src/Components/Pages/ReceptionDashBoard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReceptionHome from '../ReceptionHome'
+import Sidebar from '../ReceptionSidebar'
+import PatientTable from '../PatientTable'
 
 function ReceptionDashBoard() {
   return (
--- src/Components/PatientTable.js
+++ src/Components/PatientTable.js
@@ -2,26 +2,41 @@ import React from 'react'
 import { Fragment, useState, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+// date x
+// age
 
 
+// const people = [
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
+//     // More people...
+// ]
 
-
-const people = [
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    { name: 'Lindsay Walton', mobile: '9xxxxxxxxxx', email: 'lindsay.walton@example.com', date: '10/05/2023' },
-    // More people...
-]
-
-
+const getURL = "http://localhost:4000/patient"
 
 function PatientTable() {
     const [openEdit, setOpenEdit] = useState(false)
     const cancelButtonRef = useRef(null)
+
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        axios.get(getURL)
+            .then((response) => {
+                console.log(response);
+                setPeople(response.data)
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    })
 
     return (
         <div className="py-4">
@@ -50,7 +65,7 @@ function PatientTable() {
                                     <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                                         <table className="min-w-full divide-y divide-gray-300">
                                             <thead className="bg-gray-50">
-                                                <tr>
+                                            <tr>
                                                     <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
                                                         Name
                                                     </th>
@@ -61,41 +76,46 @@ function PatientTable() {
                                                         Email
                                                     </th>
                                                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                        Date
+                                                        Gender
                                                     </th>
                                                     <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                                        <span className="sr-only">Edit</span>
+                                                        Age
+                                                    </th>
+                                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                        Address
+                                                    </th>
+                                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                        Date
                                                     </th>
                                                 </tr>
                                             </thead>
                                             <tbody className="bg-white">
-                                                {people.map((person, personIdx) => (
+                                            {people.map((person, personIdx) => (
                                                     <tr key={person.email} className={personIdx % 2 === 0 ? undefined : 'bg-gray-50'}>
                                                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                                                             {person.name}
                                                         </td>
-                                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.mobile}</td>
+                                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.phone}</td>
                                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.email}</td>
-                                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.date}</td>
-
+                                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.gender}</td>
                                                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                            <a href="#" className="text-indigo-600 hover:text-indigo-900" onClick={
-                                                                () => {
-                                                                    setOpenEdit(true)
-                                                                }
-                                                            }>
-                                                                Edit
-                                                            </a>
+                                                            {person.age}
+                                                        </td>
+                                                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                            {person.address}
+                                                        </td>
+                                                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                            {person.date_registration}
                                                         </td>
                                                     </tr>
                                                 ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                                            </tbody >
+                                        </table >
+                                    </div >
+                                </div >
+                            </div >
+                        </div >
+                    </div >
                     {/* <Transition.Root show={openEdit} as={Fragment}>
                         <Dialog
                             as="div"
@@ -176,7 +196,7 @@ function PatientTable() {
                             </div>
                         </Dialog>
                     </Transition.Root> */}
-                    <Transition.Root show={openEdit} as={Fragment}>
+                    < Transition.Root show={openEdit} as={Fragment}>
                         <Dialog
                             as="div"
                             className="relative z-30"
@@ -320,10 +340,10 @@ function PatientTable() {
                                 </div>
                             </div>
                         </Dialog>
-                    </Transition.Root>
+                    </Transition.Root >
                 </>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
