a746e1d4d56e64067e5325145e04880dd9f8b582
Ankit Savani <ankitmsavani12345@gmail.com>
2023-08-15 19:39:43 +0530

    Fix typo warning
    Signed-off-by: Ankit Savani <ankitmsavani12345@gmail.com>

--- src/Components/PatientTable.js
+++ src/Components/PatientTable.js
@@ -297,7 +297,7 @@ function PatientTable() {
                                                     </div>
                                                 </div>
                                             </form>
-                                            <div className="bg-gray-50 px-4 py-3 flex flex-row-reverse px-6 rounded-xl gap-x-5">
+                                            <div className="bg-gray-50 py-3 flex flex-row-reverse px-6 rounded-xl gap-x-5">
                                                 <button
                                                     type="submit"
                                                     className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
