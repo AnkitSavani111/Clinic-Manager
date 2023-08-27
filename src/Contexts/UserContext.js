11c32ce8c3ed1ffb42c6acd7def44ee3f0914772
abhi-rupareliya <abhirupareliya25@gmail.com>
2023-08-27 12:48:22 +0530

    Login fetch initial

---
+++ src/Contexts/UserContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  let user = {};
+  const setUser = (newUser) => {
+    user = newUser;
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
