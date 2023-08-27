76175849ad3df0e3814819666dedce5f990184c3
abhi-rupareliya <abhirupareliya25@gmail.com>
2023-08-27 12:48:22 +0530

    Login fetch initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import './App.css';
-import Login from './Components/Login';
-import Home from './Components/Pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Appointment from './Components/Appointment';
-import About from './Components/About';
-import SignUp from './Components/SignUp';
-import ReceptionDashBoard from './Components/Pages/ReceptionDashBoard';
-import Forget from './Components/Forget';
-
+import "./App.css";
+import Login from "./Components/Login";
+import Home from "./Components/Pages/Home";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Appointment from "./Components/Appointment";
+import About from "./Components/About";
+import SignUp from "./Components/SignUp";
+import ReceptionDashBoard from "./Components/Pages/ReceptionDashBoard";
+import Forget from "./Components/Forget";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useUser } from "../src/Contexts/UserContext";
 
 function App() {
+  // const [userData, setUserData] = useState(null);
+  const { user, setUser } = useUser();
+  const navigate = useNavigate();
+
+  axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = "http://localhost:4000";
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const response = await axios.get("/user/verify");
+        setUser({
+          _id: response.data._id,
+          email: response.data.email,
+          role: response.data.role,
+        });
+        // setUserData(response.data);
+      } catch (error) {
+        // console.warn("Unauthorized");
+        navigate("/login");
+      }
+    };
+    verifyUser();
+    console.warn(user);
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/appointment' element={<Appointment />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/receptiondashb' element={<ReceptionDashBoard />} />
-          <Route path='/forget' element={<Forget />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        {user ? (
+          <>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/appointment' element={<Appointment />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/receptiondashb' element={<ReceptionDashBoard />} />
+          </>
+        ) : (
+          <></>
+        )}
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/forget' element={<Forget />} />
+      </Routes>
     </>
   );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,18 +2,46 @@ import React, { useState } from "react";
 // import DoctorLogin from "./DoctorLogin";
 // import ReceptionLogin from "./ReceptionLogin";
 import Logo from "../Images/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useUser } from "../Contexts/UserContext";
 // import { Input } from "@mui/material";
 
 const Login = () => {
   const [comp, setComp] = useState("Doctor");
-  const [Role, setRole] = useState("")
+  const [Role, setRole] = useState("");
+  const navigate = useNavigate();
+  const { user, setUser } = useUser();
   const handleDoctor = () => {
     setComp("Doctor");
   };
 
   const handleReception = () => {
     setComp("Reception");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(Role);
+    try {
+      axios.defaults.baseURL = "http://localhost:4000";
+      axios.withCredentials = true;
+      const res = await axios.post("/user/login", {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        rememberMe: true,
+      });
+
+      // console.warn(res.data);
+      setUser({
+        _id: res.data.user._id,
+        email: res.data.user.email,
+        role: res.data.user.role,
+      });
+      navigate("/");
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
@@ -59,106 +87,109 @@ const Login = () => {
         </div>
       </div> */}
 
-      <div className="Background bg-slate-400 bg-cover relative h-screen">
-
-        <div className="flex justify-center items-center h-screen">
-
-          <div className="Card bg-slate-300 flex sm:w-2/5 sm:h-auto h-4/5 flex-col justify-center m-auto items-center shadow-custom6 drop-shadow-md py-10 sm:px-6 lg:px-8 backdrop-blur-lg rounded-2xl">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md mix-blend-multiply">
+      <div className='Background bg-slate-400 bg-cover relative h-screen'>
+        <div className='flex justify-center items-center h-screen'>
+          <div className='Card bg-slate-300 flex sm:w-2/5 sm:h-auto h-4/5 flex-col justify-center m-auto items-center shadow-custom6 drop-shadow-md py-10 sm:px-6 lg:px-8 backdrop-blur-lg rounded-2xl'>
+            <div className='sm:mx-auto sm:w-full sm:max-w-md mix-blend-multiply'>
               <img
-                className="sm:m-auto m-auto mt-5 h-12 w-auto mix-blend-multiply rounded-md"
+                className='sm:m-auto m-auto mt-5 h-12 w-auto mix-blend-multiply rounded-md'
                 src={Logo}
-                alt="Your Company"
+                alt='Your Company'
               />
-              <h2 className="mt-6 text-center sm:text-3xl text-2xl font-bold tracking-tight text-gray-900">
+              <h2 className='mt-6 text-center sm:text-3xl text-2xl font-bold tracking-tight text-gray-900'>
                 Sign in to your account
               </h2>
             </div>
 
-            <div className="sm:mt-5 sm:mx-auto sm:w-full sm:max-w-md">
-              <div className="bg-transparent py-8 px-4 sm:rounded-lg sm:px-10">
-                <form className="space-y-6" action="#" method="POST">
+            <div className='sm:mt-5 sm:mx-auto sm:w-full sm:max-w-md'>
+              <div className='bg-transparent py-8 px-4 sm:rounded-lg sm:px-10'>
+                <form className='space-y-6' onSubmit={handleSubmit}>
                   <div>
                     <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
+                      htmlFor='email'
+                      className='block text-sm font-medium text-gray-700'
                     >
                       Email address
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
+                        id='email'
+                        name='email'
+                        type='email'
+                        autoComplete='email'
                         required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        className='block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                       />
                     </div>
                   </div>
 
                   <div>
                     <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700"
+                      htmlFor='password'
+                      className='block text-sm font-medium text-gray-700'
                     >
                       Password
                     </label>
-                    <div className="mt-1">
+                    <div className='mt-1'>
                       <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
+                        id='password'
+                        name='password'
+                        type='password'
+                        autoComplete='current-password'
                         required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        className='block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                       />
                     </div>
                   </div>
 
                   <div>
                     <div>
-                      <label htmlFor="Role" className="block mb-2 text-sm font-medium text-gray-900">Select your work</label>
+                      <label
+                        htmlFor='Role'
+                        className='block mb-2 text-sm font-medium text-gray-900'
+                      >
+                        Select your work
+                      </label>
                       <select
-                        onChange={(e) => { setRole(e.target.value) }}
+                        onChange={(e) => {
+                          setRole(e.target.value);
+                        }}
                         value={Role}
-                        name="Role"
-                        id="Role"
+                        name='Role'
+                        id='Role'
                         className='inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100'
                       >
-                        <option value="" disabled selected>
+                        <option value='' disabled selected>
                           Select Role
                         </option>
-                        <option value="doctor">
-                          Doctor
-                        </option>
-                        <option value="receptionist">
-                          Receptionist
-                        </option>
+                        <option value='doctor'>Doctor</option>
+                        <option value='receptionist'>Receptionist</option>
                       </select>
-
                     </div>
                   </div>
 
-                  <div className="sm:flex sm:items-center sm:justify-between items-center p-1">
-                    <div className="flex items-center">
+                  <div className='sm:flex sm:items-center sm:justify-between items-center p-1'>
+                    <div className='flex items-center'>
                       <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        id='remember-me'
+                        name='remember-me'
+                        type='checkbox'
+                        className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
                       />
                       <label
-                        htmlFor="remember-me"
-                        className="sm:ml-2 sm:font-medium font-medium sm:block sm:text-[15px] text-sm ml-1 text-black"
+                        htmlFor='remember-me'
+                        className='sm:ml-2 sm:font-medium font-medium sm:block sm:text-[15px] text-sm ml-1 text-black'
                       >
                         Remember me
                       </label>
                     </div>
 
-                    <div className="text-sm mt-5 sm:mt-0"> {/* Add mt-2 to create space on mobile */}
-                      <Link to="/forget"
-                        className="sm:font-medium font-medium sm:text-[14px] text-indigo-900 sm:text-indigo-900 sm:hover:text-slate-950 sm:duration-100"
+                    <div className='text-sm mt-5 sm:mt-0'>
+                      {" "}
+                      {/* Add mt-2 to create space on mobile */}
+                      <Link
+                        to='/forget'
+                        className='sm:font-medium font-medium sm:text-[14px] text-indigo-900 sm:text-indigo-900 sm:hover:text-slate-950 sm:duration-100'
                       >
                         Forgot your password ?
                       </Link>
@@ -167,8 +198,8 @@ const Login = () => {
 
                   <div>
                     <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      type='submit'
+                      className='flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                     >
                       Sign in
                     </button>
@@ -178,7 +209,6 @@ const Login = () => {
             </div>
           </div>
         </div>
-
       </div>
     </>
   );
