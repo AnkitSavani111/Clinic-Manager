4ba3f45d5076233206727d9fa3fcbc0519e4a2de
Divy Sheta <shetadivy@gmail.com>
2023-09-01 23:50:11 +0530

    Some changes in Homepage

--- src/Components/Homebody.js
+++ src/Components/Homebody.js
@@ -11,7 +11,7 @@ function Homebody() {
             <main className='-z-10'>
                 <div id='home' className="relative px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:my-auto">
-                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                        {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                                 Announcing our next round of funding.
                                 <Link to="/" className="font-semibold text-indigo-600">
@@ -19,25 +19,27 @@ function Homebody() {
                                     Read more <span aria-hidden="true">&rarr;</span>
                                 </Link>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                Data to enrich your online business
+                                Ashirwad Dental Clinic
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                                fugiat veniam occaecat fugiat aliqua.
+                                "Smile Brighter: Your Geteway to Dental Excellence"
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <Link
-                                    to="/"
+                                    to="/Appointment"
                                     className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-                                    Get started
+                                    Book Appointment
                                 </Link>
-                                <Link to="/" className="text-base font-semibold leading-7 text-gray-900">
-                                    Learn more <span aria-hidden="true">→</span>
-                                </Link>
+                                {/* <Link to="#cause-heading" className="text-base font-semibold leading-7 text-gray-900">
+                                    Know More <span aria-hidden="true">→</span>
+                                </Link> */}
+                                <a href="#cause-heading" className="text-base font-semibold leading-7 text-gray-900 scroll-smooth md:scroll-auto">
+                                    Know More <span aria-hidden="true">→</span>
+                                </a>
                             </div>
                         </div>
                     </div>
