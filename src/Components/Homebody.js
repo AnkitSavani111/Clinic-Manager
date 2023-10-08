dde51afd6fca0c231af3b470e2c71839bec73dd1
JaivinSavaliya <jpsavaliya14@gmail.com>
2023-10-08 18:49:45 +0530

    new changes adapted

--- src/Components/Homebody.js
+++ src/Components/Homebody.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Link } from 'react-router-dom'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
-=======
 import React from "react";
 import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
->>>>>>> main
 
 function Homebody() {
   return (
